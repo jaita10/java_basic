2d6f7cefcfe01711c225d3043d4243d0020447e7
HP <HP@DESKTOP-V7E8M36>
2022-03-22 02:01:52 +0530

    Adding File

---
+++ src/com/training/cg/FirstDemo.java
@@ -0,0 +1,10 @@
+package com.training.cg;
+
+public class FirstDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+	}
+
+}
